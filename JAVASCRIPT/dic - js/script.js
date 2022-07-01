5adc253be7f7416414293d5ff0ef22fb27ff2d36
unknown <devsarodrigues@gmail.com>
2022-07-01 11:07:50 -0300

    inserção de novos arquivos aleatórios.

---
+++ JAVASCRIPT/dic - js/script.js
@@ -0,0 +1,86 @@
+//tipos primitivos
+
+//boleano
+/*var vOuF = false;
+console.log(typeof (vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof (numeroQualquer));
+
+//string ou caractere
+var nome = 'samantha';
+console.log(typeof (nome));
+
+//function
+var funcao = function(){}
+console.log(typeof (funcao));*/
+
+//como declarar
+/*var variável = 'samantha';
+variável = 'rodrigues'
+console.log(variável)*/
+
+/*let variavel2 = 'samantha';
+variavel2 = 'rodrigues';
+console.log(variavel2);*/
+
+/*const constante = 'samantha';
+constante = 'rodrigues'; // não pde ser alterada por ser uma constante
+console.log(constante);*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();*/
+
+//atribuição
+/*var atribuicao = 'samantha';
+
+//comparação;;
+var comparacao = '0'== 0;
+console.log(comparacao);
+
+//comparação identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+//adição
+/*var adicao = 1 + 1;
+console.log(adicao);
+
+//subtracao
+var subtracao = 1 - 1;
+console.log(subtracao);
+
+//multiplocação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 4 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+//potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao);*/
+
+//operadores relacionais
+
+//maior >
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+//menor <
+var menorQue = 5 < 2;
+console.log(menorQue);
+
